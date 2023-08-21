366a2a8340242429d6a18311dfac43bf22715b34
HichamBouttaj <bouttajhicham@gmail.com>
2023-08-21 20:13:09 +0100

    fix JwtAuthEntryPoint display error message

--- src/main/java/com/jwtcookie/jwttokencookie/jwt/JwtAuthEntryPoint.java
+++ src/main/java/com/jwtcookie/jwttokencookie/jwt/JwtAuthEntryPoint.java
@@ -1,18 +1,36 @@
 package com.jwtcookie.jwttokencookie.jwt;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jwtcookie.jwttokencookie.exception.ErrorDetails;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
 
 @Component
 public class JwtAuthEntryPoint implements AuthenticationEntryPoint {
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
-        response.sendError(response.SC_UNAUTHORIZED, authException.getLocalizedMessage());
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setStatus(HttpStatus.UNAUTHORIZED.value());
+
+        final Map<String, Object> body = new HashMap<>();
+        body.put("timestamp", LocalDateTime.now().toString());
+        body.put("status", HttpStatus.UNAUTHORIZED.value());
+        body.put("error", HttpStatus.UNAUTHORIZED.getReasonPhrase());
+        body.put("message", authException.getMessage());
+        body.put("details", request.getServletPath());
+
+        final ObjectMapper mapper = new ObjectMapper();
+        mapper.writeValue(response.getOutputStream(), body);
     }
 }
